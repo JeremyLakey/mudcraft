5a782325d7ec6cf990e264180951670240fee4c2
personman61 <jeremysl123@gmail.com>
2024-02-27 22:36:11 -0700

    Update mudcraft.js

--- mudcraft.js
+++ mudcraft.js
@@ -18,7 +18,7 @@ const bot = mineflayer.createBot({
   username: creds.username, // username or email, switch if you want to change accounts
   auth: 'offline',//'microsoft' // for offline mode servers, you can set this to 'offline'
   port: 64690,                // only set if you need a port that isn't 25565
-  version: "1.20",             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  version: "1.20.1",             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'        // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
